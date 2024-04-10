bef9e3488e269d7333c85134872a336fe197129c
Malva <malvaorbitadigital@gmail.com>
2024-04-10 10:32:31 +0200

    ejercicio 1

--- e1/index.php
+++ e1/index.php
@@ -1,2 +1,33 @@
 <?php
 
+$texto = "Comer algas es realmente sano";
+echo $texto;
+echo "<br>";
+$textoArr = explode(" ", $texto);
+
+$posAlgas = array_search("algas", $textoArr, true);
+echo "Posición 'algas': " . $posAlgas;
+echo "<br>";
+
+$texto = str_replace("realmente", "muy", $texto);
+echo $texto;
+echo "<br>";
+
+$contieneAnacardo = str_contains($texto, "anacardo");
+echo "Contiene anarcardo: ";
+echo $contieneAnacardo?"sí":"no";
+echo "<br>";
+
+$texto = strrev($texto);
+echo $texto;
+echo "<br>";
+
+$numE = count_chars($texto);
+
+foreach (count_chars($texto, 1) as $i => $val) {
+    if (chr($i) == 'e'){
+        echo "Hay $val \"" , chr($i) , "\" en el texto\n";
+    }
+ }
+
+?>
